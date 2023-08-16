9eb5292b60b38f4d693e46749c6f5fc166c5b086
Diego Alencar Jacober <57761933898>
2023-08-16 14:12:09 -0300

    Criando projeto

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native'
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <StatusBar backgroundColor='#f0f4ff' barStyle='dark-content'/>
+      <Routes />
+    </NavigationContainer>
+  );
+}
+
